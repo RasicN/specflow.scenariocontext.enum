de0e5f71cea19c18cbde1eabb8cfd93fdb289aa8
jncisar <joe.cisar@ryvit.com>
2019-11-01 08:45:22 -0500

    added extension class

---
+++ Specflow.ScenarioContext.Enum/ScenarioContextExtensionsEnum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Specflow.ScenarioContext.Enum
+{
+    public static class ScenarioContextExtensionsEnum
+    {
+        public static T Get<T>(this TechTalk.SpecFlow.ScenarioContext context, IConvertible key)
+        {
+            return context.Get<T>(key.ToString(CultureInfo.InvariantCulture));
+        }
+
+        public static void Set(this TechTalk.SpecFlow.ScenarioContext context, object data, IConvertible key)
+        {
+            context.Set(data, key.ToString(CultureInfo.InvariantCulture));
+        }
+
+        public static bool TryGetValue<T>(this TechTalk.SpecFlow.ScenarioContext context, IConvertible key, out T value)
+        {
+            value = default;
+            if (context.TryGetValue<T>(key.ToString(CultureInfo.InvariantCulture), out var x) == false)
+            {
+                return false;
+            }
+
+            value = x;
+            return true;
+        }
+
+        public static bool ContainsKey(this TechTalk.SpecFlow.ScenarioContext context, IConvertible key)
+        {
+            return context.ContainsKey(key.ToString(CultureInfo.InvariantCulture));
+        }
+    }
+}
